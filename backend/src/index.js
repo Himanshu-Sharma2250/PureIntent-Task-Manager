7a50a23210351eb5d8b8425d022c1cf8fb72baf9
Himanshu-Sharma2250 <him221901250@gmail.com>
2026-06-06 10:56:39 +0530

    fix: add cors options for more security

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,8 +16,10 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }))
 
 app.get("/", (req, res) => {
